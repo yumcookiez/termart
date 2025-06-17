7f1067f1ee5d716f1f53714e2562e58708db583d
yumcookiez <yumcookiez@proton.me>
2025-06-17 22:40:07 +1000

    Improve termart.lua

--- termart.lua
+++ termart.lua
@@ -18,15 +18,13 @@ end
 function show_album_art(path)
     local tmp_img = "/tmp/mpv_albumart.jpg"
 
-    os.remove(tmp_img)
-
     local extract_cmd = {
         "ffmpeg", "-loglevel", "quiet", "-y", "-i", path, "-an", "-vcodec", "copy", tmp_img
     }
-    local result = utils.subprocess({args = extract_cmd, cancellable = false})
+    local result = utils.subprocess({args = extract_cmd})
 
     if result.status == 0 then
-        local chafa_cmd = {"chafa", "-s", "20x20", tmp_img}
+        local chafa_cmd = {"chafa", "--size", "20x20", "--symbols", "all", tmp_img}
         mp.command_native_async({name = "subprocess", args = chafa_cmd, playback_only = false}, function(_) end)
     else
         msg.warn("Album art not found")
